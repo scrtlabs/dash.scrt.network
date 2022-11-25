c3235199732f89b3b4040ee24ef1640ab772d720
h3rt <94856309+SecretSaturn@users.noreply.github.com>
2022-11-25 02:05:36 +0100

    Fix Keplr with use context, dark theme for notifications and code cleanup

--- src/layouts/defaultLayout.tsx
+++ src/layouts/defaultLayout.tsx
@@ -1,11 +1,13 @@
 import { Footer } from 'components/Footer';
 import { KeplrPanel } from 'components/Keplr';
-import { Navigation } from 'components/Navigation';
-import React, { useState } from 'react';
+import { Navigation } from 'components/navigation';
+import React, { useState, createContext} from 'react';
 import { SecretNetworkClient } from 'secretjs';
 
-const DefaultLayout =({children}) =>{
-  const [secretjs, setSecretjs] = useState<SecretNetworkClient | null>(null);
+export const KeplrContext = createContext(null);
+
+export const DefaultLayout =({children}) =>{
+  const [secretjs, setSecretjs] = useState<SecretNetworkClient | null >(null);
   const [secretAddress, setSecretAddress] = useState<string>("");
     return(
         <>
@@ -14,6 +16,12 @@ const DefaultLayout =({children}) =>{
             <aside className="w-72 fixed left-0 top-0 h-screen bg-zinc-800 p-10 overflow-x-hidden">
               <Navigation />
             </aside>
+            <KeplrContext.Provider
+                 value={{
+                  secretjs,
+                  secretAddress,
+                }}
+               >
             <main className="flex-1 ml-72">
               {/* Keplr */}
               <div className="md:flex md:justify-end m-4">
@@ -28,6 +36,7 @@ const DefaultLayout =({children}) =>{
               {children}
               <Footer />
             </main>
+            </KeplrContext.Provider>
           </div>
         </>
     )
