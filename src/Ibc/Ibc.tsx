f001c950632f836f41db6528d1621e17f8e4165c
Kevin Rieger <mail@kevinrieger.com>
2022-12-11 13:17:40 +0100

    updated

--- src/Ibc/Ibc.tsx
+++ src/Ibc/Ibc.tsx
@@ -3,71 +3,41 @@ import { chains, Token, tokens } from "General/Utils/config";
 import { toast} from "react-toastify";
 import Deposit from "./components/Deposit";
 import Withdraw from "./components/Withdraw";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 export function Ibc() {
-  enum IbcMode {
-    Deposit,
-    Withdrawal
-  }
-
   const [balances, setBalances] = useState<Map<string, string>>(new Map());
   const [selectedToken, setselectedToken] = useState<Token>(tokens.filter(token => token.name === "SCRT")[0]);
 
-  const [ibcMode, setIbcMode] = useState<IbcMode>(IbcMode.Deposit);
-
   function Header() {
-    return <>
+    return (
       <div className="mb-4">
-        <h1 className="inline text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-violet-500">
-          IBC Transfer
-        </h1>
+        <div className="mb-4">
+          <h1 className="inline text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-violet-500">
+            IBC Transfer
+          </h1>
+        </div>
+        <span className="block">
+          Transfer your tokens via IBC (Inter-Blockchain Communication)
+        </span>
       </div>
-      <span className="block mb-4">
-        Transfer your tokens via IBC (Inter-Blockchain Communication)
-      </span>
-    </>
-  }
+    )}
 
   return (
     <div className="w-full max-w-xl mx-auto px-4">
-      <div className="border rounded-lg border-zinc-700 w-full bg-zinc-800 text-zinc-200">
-      <Header> </Header>
-        {/* <Header/> */}
-        
-        {/* [Deposit|Withdrawal] */}
-        <div className="flex">
-          <button onClick={() => setIbcMode(IbcMode.Deposit)} className={"flex-1 py-4" + (ibcMode === IbcMode.Deposit ? "  bg-zinc-800 rounded-tl-lg cursor-default" : "  bg-zinc-700/50 rounded-tl-lg hover:bg-zinc-600 transition-colors")}>
-          <h1 className={ibcMode === IbcMode.Deposit ? "inline text-lg font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-violet-500" : "text-lg"}>Deposit</h1>
-          </button>
-          <button onClick={() => setIbcMode(IbcMode.Withdrawal)} className={"flex-1 py-4" + (ibcMode === IbcMode.Withdrawal ? "  bg-zinc-800 rounded-tl-lg cursor-default" : "  bg-zinc-700/50 rounded-tr-lg hover:bg-zinc-600 transition-colors")}>
-            <h1 className={ibcMode === IbcMode.Withdrawal ? "inline text-lg font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-violet-500" : "text-lg font-medium"}>Withdrawal</h1>
-          </button>
-        </div>
+      <div className="border rounded-lg p-12 pb-7 border-zinc-700 w-full bg-zinc-800 text-zinc-200">
+        <Header />
 
-        <div className="px-12 pb-7">
           {/* Deposit */}
-          {ibcMode === IbcMode.Deposit &&
-            <Deposit
-              token={selectedToken}
-              onSuccess={(txhash) => {
-                console.log("success", txhash);
-              }}
-              onFailure={(error) => console.error(error)}
-            />
-          }
-
-          {/* Withdrawal */}
-          {ibcMode === IbcMode.Withdrawal &&
-            <Withdraw
-              token={selectedToken}
-              balances={balances}
-              onSuccess={(txhash) => {
-                console.log("success", txhash);
-              }}
-              onFailure={(error) => console.error(error)}
-            />
-          }
-        </div>
+          <Deposit
+            token={selectedToken}
+            tokens={tokens}
+            onSuccess={(txhash) => {
+              console.log("success", txhash);
+            }}
+            onFailure={(error) => console.error(error)}
+          />
       </div>
     </div>
   );
