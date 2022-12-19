a6a0872ccc4eeaa11c6af20edcf56a595554f9c3
Kevin Rieger <mail@kevinrieger.com>
2022-12-19 12:43:08 +0100

    updated

--- src/Dashboard/Components/PriceChart.tsx
+++ src/Dashboard/Components/PriceChart.tsx
@@ -64,7 +64,31 @@ export default function PriceChart() {
   };
 
   return (
+    <>
+    <div className="flex items-center mb-4">
+      <div className="flex-1 inline-flex rounded-md shadow-sm" role="group">
+        <button type="button" className="py-1.5 px-3 text-xs font-medium text-zinc-200 bg-zinc-700 rounded-l-lg border border-zinc-400 hover:bg-zinc-500 hover:text-white focus:z-10 focus:ring-2 focus:ring-zinc-700 focus:text-white">
+          Price
+        </button>
+        <button type="button" className="py-1.5 px-3 text-xs font-medium text-zinc-200 bg-zinc-700 rounded-r-md border-t border-b border-r border-zinc-400 hover:bg-zinc-500 hover:text-white focus:z-10 focus:ring-2 focus:ring-zinc-700 focus:text-white">
+          Volume
+        </button>
+      </div>
+
+      <div className="flex-initial inline-flex rounded-md shadow-sm" role="group">
+        <button type="button" className="py-1.5 px-3 text-xs font-medium text-zinc-200 bg-zinc-700 rounded-l-lg border border-zinc-400 hover:bg-zinc-500 hover:text-white focus:z-10 focus:ring-2 focus:ring-zinc-700 focus:text-white">
+          Day
+        </button>
+        <button type="button" className="py-1.5 px-3 text-xs font-medium text-zinc-200 bg-zinc-700 border-t border-b border-zinc-400 hover:bg-zinc-500 hover:text-white focus:z-10 focus:ring-2 focus:ring-zinc-700 focus:text-white">
+          Month
+        </button>
+        <button type="button" className="py-1.5 px-3 text-xs font-medium text-zinc-200 bg-zinc-700 rounded-r-md border border-zinc-400 hover:bg-zinc-500 hover:text-white focus:z-10 focus:ring-2 focus:ring-zinc-700 focus:text-white">
+          Year
+        </button>
+      </div>
+    </div>
     <Line data={data} options={options}/>
+    </>
   );
 }
 function componentDidMount() {
