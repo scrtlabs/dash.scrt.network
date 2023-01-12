0e0039ad2029bd87f3c07a83cd7b2c1662f77668
Kevin Rieger <kevin.rieger@crownpeak.com>
2023-01-12 17:36:24 +0100

    updated

--- src/Dashboard/Components/CommunityPool.tsx
+++ src/Dashboard/Components/CommunityPool.tsx
@@ -1,9 +1,14 @@
-import { faArrowTrendUp, faAward, faCircle, faCube } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowTrendUp,
+  faAward,
+  faCircle,
+  faCube,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 interface ICommunityPoolProps {
-  amount: number
+  amount: number;
 }
 
 const CommunityPool = (props: ICommunityPoolProps) => {
@@ -11,19 +16,27 @@ const CommunityPool = (props: ICommunityPoolProps) => {
     <>
       {/* Title */}
       {props.amount && (
-        <div className="col-span-12 sm:col-span-6  xl:col-span-3 bg-neutral-800 p-4 rounded-lg h-full">
-          <div className="flex flex-col items-center">
-            <span className="fa-stack fa-2x mb-2">
-              <FontAwesomeIcon icon={faCircle} className="fa-stack-2x text-purple-900" />
-              <FontAwesomeIcon icon={faAward} className="fa-stack-1x fa-inverse text-purple-400" />
+        <div className='col-span-12 sm:col-span-6  xl:col-span-3 bg-neutral-800 p-4 rounded-lg h-full'>
+          <div className='flex flex-col items-center'>
+            <span className='fa-stack fa-2x mb-2'>
+              <FontAwesomeIcon
+                icon={faCircle}
+                className='fa-stack-2x text-purple-900'
+              />
+              <FontAwesomeIcon
+                icon={faAward}
+                className='fa-stack-1x fa-inverse text-purple-400'
+              />
             </span>
-            <div className="font-semibold text-lg">{props.amount ? props.amount.toLocaleString() : ""} SCRT</div>
-            <div className="text-md text-neutral-400">Community Pool</div>
+            <div className='font-semibold text-lg'>
+              {props.amount ? props.amount.toLocaleString() : ""} SCRT
+            </div>
+            <div className='text-md text-neutral-400'>Community Pool</div>
           </div>
         </div>
       )}
     </>
   );
-}
+};
 
 export default CommunityPool;
