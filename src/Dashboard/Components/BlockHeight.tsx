82f1a0a5dc2e31eb78915aab1e38bc8fec35c175
Kevin Rieger <kevin.rieger@crownpeak.com>
2023-01-12 17:32:04 +0100

    updated

--- src/Dashboard/Components/BlockHeight.tsx
+++ src/Dashboard/Components/BlockHeight.tsx
@@ -3,27 +3,35 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 interface IBlockHeightProps {
-  blockHeight: number
+  blockHeight: number;
 }
 
-const BlockHeight = (props: IBlockHeightProps) => {
-  return (
-    <>
-      {/* Title */}
-      {props.blockHeight && (
-        <div className="col-span-12 sm:col-span-6  xl:col-span-3 bg-neutral-800 p-4 rounded-lg h-full">
-          <div className="flex flex-col items-center">
-            <span className="fa-stack fa-2x mb-2">
-              <FontAwesomeIcon icon={faCircle} className="fa-stack-2x text-cyan-900" />
-              <FontAwesomeIcon icon={faCube} className="fa-stack-1x fa-inverse text-cyan-400" />
+class BlockHeight extends React.Component<IBlockHeightProps> {
+  render() {
+    return (
+      <>
+        {/* Title */}
+        <div className='col-span-12 sm:col-span-6  xl:col-span-3 bg-neutral-800 p-4 rounded-lg h-full'>
+          <div className='flex flex-col items-center'>
+            <span className='fa-stack fa-2x mb-2'>
+              <FontAwesomeIcon
+                icon={faCircle}
+                className='fa-stack-2x text-cyan-900'
+              />
+              <FontAwesomeIcon
+                icon={faCube}
+                className='fa-stack-1x fa-inverse text-cyan-400'
+              />
             </span>
-            <div className="font-semibold text-lg">{props.blockHeight}</div>
-            <div className="text-md text-neutral-400">Block Height</div>
+            <div className='font-semibold text-lg'>
+              {this.props.blockHeight}
+            </div>
+            <div className='text-md text-neutral-400'>Block Height</div>
           </div>
         </div>
-      )}
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default BlockHeight;
