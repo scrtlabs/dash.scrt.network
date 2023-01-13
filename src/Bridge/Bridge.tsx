4f79e894e7a23e9e28889a8b8c21a6d16eab3cdd
h3rt <alexander.hertlein@icloud.com>
2023-01-13 12:51:29 +0100

    update

--- src/Bridge/Bridge.tsx
+++ src/Bridge/Bridge.tsx
@@ -18,17 +18,9 @@ class Bridge extends React.Component {
         <div className='max-w-2xl mx-auto px-6'>
           <h1 className='text-center font-bold text-4xl mb-10'>Bridge</h1>
           <p className='text-neutral-400'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi,
-            libero! Ipsum similique a laudantium repellat amet modi harum quos!
-            Reiciendis dolore doloribus aspernatur odit ratione vero corrupti
-            deserunt modi tempore.
+            Use the Axelar bridge down below to bridge your assets from Ethereum, Binance Smart chain (BSC), Arbitrum, Avalance, Moonbeam or Polygon to Secret.
+            After bridging from Axelar, your bridged assets are all publicly visible. You will need to wrap them using the Secret Wrap.
           </p>
-          <Link
-            to='/wrap'
-            className='block my-2 p-3 w-full text-center font-semibold bg-cyan-600 rounded-lg text-sm hover:bg-cyan-500 focus:bg-cyan-600 transition-colors'
-          >
-            Secret Wrap
-          </Link>
           <a
             href='https://satellite.money/?asset_denom=uusdc&source=ethereum&destination=secret'
             target='_blank'
@@ -37,6 +29,15 @@ class Bridge extends React.Component {
             Axelar Bridge
             <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='ml-2' />
           </a>
+          <p className='text-neutral-400'>
+            Done with bridging into Secret ? Wrap your publicly visible assets with Secret Wrap into its privacy-preserving equivalent.
+          </p>
+          <Link
+            to='/wrap'
+            className='block my-2 p-3 w-full text-center font-semibold bg-cyan-600 rounded-lg text-sm hover:bg-cyan-500 focus:bg-cyan-600 transition-colors'
+          >
+            Secret Wrap
+          </Link>
         </div>
       </>
     );
