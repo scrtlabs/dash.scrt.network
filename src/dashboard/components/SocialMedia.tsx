1e885bcae6620c25c1a9eb7d275e44678da1b16d
Kevin Rieger <mail@kevinrieger.com>
2023-01-15 20:23:27 +0100

    added darkmode implementation

--- src/dashboard/components/SocialMedia.tsx
+++ src/dashboard/components/SocialMedia.tsx
@@ -6,12 +6,12 @@ class SocialMedia extends React.Component {
   render() {
     return (
       <>
-        <div className='bg-neutral-800 rounded-xl h-full flex items-center px-8 py-4'>
-          <div className='flex-1 text-right pr-4 border-r border-neutral-700'>
+        <div className='bg-white dark:bg-neutral-800 rounded-xl h-full flex items-center px-8 py-4'>
+          <div className='flex-1 text-right pr-4 border-r border-neutral-300 dark:border-neutral-700'>
             <a
               href='https://twitter.com/SecretNetwork'
               target='_blank'
-              className='text-neutral-300 hover:text-white transition-colors'
+              className='text-neutral-700 dark:text-neutral-300 hover:text-black dark:hover:text-white transition-colors'
             >
               <FontAwesomeIcon icon={faTwitter} size='xl' className='mr-2' />
               <span className='text-md font-medium'>Twitter</span>
@@ -21,7 +21,7 @@ class SocialMedia extends React.Component {
             <a
               href='https://discord.com/invite/SJK32GY'
               target='_blank'
-              className='text-neutral-300 hover:text-white transition-colors'
+              className='text-neutral-700 dark:text-neutral-300 hover:text-black dark:hover:text-white transition-colors'
             >
               <FontAwesomeIcon icon={faDiscord} size='xl' className='mr-2' />
               <span className='text-md font-medium'>Discord</span>
