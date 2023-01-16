cc3257a82e24e2362b3e30fbd4f39b822b9620a1
Kevin Rieger <kevin.rieger@crownpeak.com>
2023-01-16 14:28:53 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { Window as KeplrWindow } from "@keplr-wallet/types";
 import { BreakpointProvider } from "react-socks";
 import "shared/assets/scss/index.scss";
@@ -28,7 +28,10 @@ window.addEventListener("keplr_keystorechange", () => {
   location.reload();
 });
 
-class ErrorBoundary extends React.Component<{children: any}, { hasError: boolean }> {
+class ErrorBoundary extends React.Component<
+  { children: any },
+  { hasError: boolean }
+> {
   constructor(props: any) {
     super(props);
     this.state = { hasError: false };
@@ -55,9 +58,12 @@ class ErrorBoundary extends React.Component<{children: any}, { hasError: boolean
   }
 }
 
+export const ThemeContext = createContext(null);
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <BreakpointProvider>
     <HelmetProvider>
@@ -73,18 +79,37 @@ root.render(
 );
 
 export default function App() {
+  /**
+   * Dark Mode Switch
+   */
+  type Theme = "light" | "dark";
+  const [theme, setTheme] = useState<Theme>("light");
+
+  const toggleTheme = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+  useEffect(() => {
+    if (theme === "light") {
+      document.body.classList.remove("dark");
+    } else {
+      document.body.classList.add("dark");
+    }
+  }, [theme]);
+
   return (
     <>
-      <Helmet>
-        <title>{websiteName}</title>
-      </Helmet>
-      <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='/ibc' element={<Ibc />} />
-        <Route path='/wrap' element={<Wrap />} />
-        <Route path='/apps' element={<Apps />} />
-        <Route path='/bridge' element={<Bridge />} />
-      </Routes>
+      <ThemeContext.Provider value={theme}>
+        <Helmet>
+          <title>{websiteName}</title>
+        </Helmet>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/ibc' element={<Ibc />} />
+          <Route path='/wrap' element={<Wrap />} />
+          <Route path='/apps' element={<Apps />} />
+          <Route path='/bridge' element={<Bridge />} />
+        </Routes>
+      </ThemeContext.Provider>
     </>
   );
 }
