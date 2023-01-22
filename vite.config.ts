5b1a10cecb896adf5eafde88e31723f6630bc2be
Kevin Rieger <mail@kevinrieger.com>
2023-01-22 14:02:41 +0100

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,4 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), tsconfigPaths()],
-  server: {
-    host: true,
-    port: 3000,
-  },
 });
