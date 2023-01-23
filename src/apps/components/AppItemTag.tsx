d2219e4a72a9f6ffb5d00952f36c807d6ed24e9f
Kevin Rieger <kevin.rieger@crownpeak.com>
2023-01-23 16:02:52 +0100

    added keys to map-renders to avoid errors

--- src/apps/components/AppItemTag.tsx
+++ src/apps/components/AppItemTag.tsx
@@ -4,7 +4,7 @@ interface ITagProps {
   name: string;
 }
 
-class AppItemTag extends React.Component<ITagProps> {
+class AppTileTag extends React.Component<ITagProps> {
   render() {
     return (
       <div className='bg-neutral-200 dark:bg-neutral-900 text-xs font-semibold px-2 py-1 rounded-md inline-block transition-colors'>
@@ -14,4 +14,4 @@ class AppItemTag extends React.Component<ITagProps> {
   }
 }
 
-export default AppItemTag;
+export default AppTileTag;
