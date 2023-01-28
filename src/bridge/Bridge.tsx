5c09d4a01fd7163868e816efae6f17717a1dd6f7
Kevin Rieger <mail@kevinrieger.com>
2023-01-28 22:21:18 +0100

    updated

--- src/bridge/Bridge.tsx
+++ src/bridge/Bridge.tsx
@@ -31,22 +31,20 @@ function Bridge() {
           >
             Axelar Bridge
           </a>{" "}
-          to bridge your assets from Blockchains such as Ethereum, Binance Smart
+          to bridge your assets from blockchains such as Ethereum, Binance Smart
           Chain (BSC), Arbitrum, Avalance, Moonbeam and Polygon to Secret
           Network!
         </p>
         <br />
         <p>
-          {/* Shh, keep in mind: After bridging your assets from Axelar Bridge, they will remain publicly visible. Wrap your assets into their privacy-preserving equivalent using <Link to={'/wrap'} className='pb-0.5 border-b border-neutral-600 hover:text-white hover:border-white transition-colors'><FontAwesomeIcon icon={faShuffle} size={'xs'} className='mr-2' />Secret Wrap</Link>! These wrapped assets require a viewing key and thus are not visible to the public by design! */}
           <span className='select-none'>
             <span className='inline-block bg-emerald-500 dark:bg-green-800 text-white text-xs py-0.5 px-1.5 rounded uppercase font-semibold'>
               Protip
             </span>{" "}
             –{" "}
           </span>
-          After bridging your assets from Axelar Bridge, they will remain
-          publicly visible. Wrap your assets into their privacy-preserving
-          equivalent using{" "}
+          After bridging your assets from Axelar Bridge, they remain publicly
+          visible. If you want to make your assets private, wrap them using
           <Link
             to={"/wrap"}
             className='pb-0.5 border-b border-neutral-400 dark:border-neutral-600 hover:text-black dark:hover:text-white hover:border-black dark:hover:border-white transition-colors'
@@ -54,15 +52,15 @@ function Bridge() {
             <FontAwesomeIcon icon={faShuffle} size={"xs"} className='mr-2' />
             Secret Wrap
           </Link>
-          ! These wrapped assets require a viewing key and thus are not visible
-          to the public by design!
+          ! Once wrapped, no one can view your assets on-chain unless they have
+          the right viewing key.
         </p>
         <a
           href='https://satellite.money/?asset_denom=uusdc&source=ethereum&destination=secret'
           target='_blank'
           className='text-white block my-6 p-3 w-full text-center font-semibold bg-cyan-600 dark:bg-cyan-600 rounded-lg text-sm hover:bg-cyan-500 dark:hover:bg-cyan-500 focus:bg-cyan-600 dark:focus:bg-cyan-600 transition-colors'
         >
-          Axelar Bridge
+          Go to the Axelar Bridge
           <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='ml-2' />
         </a>
       </div>
