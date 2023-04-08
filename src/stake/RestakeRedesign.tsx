d87ff2d86ab3aed447bb1298f3e1140d7b3d0f48
Kevin Rieger <mail@kevinrieger.com>
2023-04-08 12:45:15 +0200

    extended staking panel

--- src/stake/RestakeRedesign.tsx
+++ src/stake/RestakeRedesign.tsx
@@ -8,16 +8,24 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { websiteName } from "App";
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Helmet } from "react-helmet-async";
 import MyValidatorsItem from "./components/MyValidatorsItem";
 import AllValidatorsItem from "./components/AllValidatorsItem";
 import Tooltip from "@mui/material/Tooltip";
 import "./RestakeRedesign.scss";
+import { SecretjsContext } from "shared/context/SecretjsContext";
+import NoScrtWarning from "./components/NoScrtWarning";
 
 // for html-head
 
 function RestakeRedesign() {
+  const { secretjs, secretAddress } = useContext(SecretjsContext);
+
+  useEffect(() => {
+    alert("hi");
+  }, []);
+
   return (
     <>
       <Helmet>
@@ -31,37 +39,8 @@ function RestakeRedesign() {
         </h1>
       </div>
 
-      {/* Info no SCRT */}
-      <div className="px-4 mb-4 max-w-6xl mx-auto">
-        <div className="inline-block w-full md:w-auto bg-yellow-800/40 border border-yellow-600 text-neutral-300 p-4 rounded-lg">
-          <div className="font-semibold text-yellow-600 mb-3 flex flex-row items-center">
-            <FontAwesomeIcon icon={faTriangleExclamation} className="mr-3" />
-            <span>{`You do not have any SCRT for Staking`}</span>
-          </div>
-          <ul className="list-disc ml-4 flex flex-col gap-0.5">
-            <li>
-              <a href="" className="hover:border-b">
-                Get SCRT
-                <FontAwesomeIcon
-                  icon={faArrowUpRightFromSquare}
-                  className="text-xs ml-2"
-                  size={"xs"}
-                />
-              </a>
-            </li>
-            <li>
-              <a href="" className="hover:border-b">
-                Learn more about Staking
-                <FontAwesomeIcon
-                  icon={faArrowUpRightFromSquare}
-                  className="text-xs ml-2"
-                  size={"xs"}
-                />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      {/* TODO: Check if user has SCRT */}
+      {secretjs && secretAddress ? <NoScrtWarning /> : null}
 
       {/* My Validators */}
       <div className="my-validators mb-20 max-w-6xl mx-auto">
---
+++ src/stake/components/NoScrtWarning.tsx
@@ -0,0 +1,45 @@
+import {
+  faArrowUpRightFromSquare,
+  faTriangleExclamation,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+function NoScrtWarning() {
+  return (
+    <>
+      <div className="px-4 mb-4 max-w-6xl mx-auto">
+        <div className="inline-block w-full md:w-auto bg-yellow-800/40 border border-yellow-600 text-neutral-300 p-4 rounded-lg">
+          <div className="font-semibold text-yellow-600 mb-3 flex flex-row items-center">
+            <FontAwesomeIcon icon={faTriangleExclamation} className="mr-3" />
+            <span>{`You do not have any SCRT for Staking`}</span>
+          </div>
+          <ul className="list-disc ml-4 flex flex-col gap-0.5">
+            <li>
+              <a href="" className="hover:border-b">
+                Get SCRT
+                <FontAwesomeIcon
+                  icon={faArrowUpRightFromSquare}
+                  className="text-xs ml-2"
+                  size={"xs"}
+                />
+              </a>
+            </li>
+            <li>
+              <a href="" className="hover:border-b">
+                Learn more about Staking
+                <FontAwesomeIcon
+                  icon={faArrowUpRightFromSquare}
+                  className="text-xs ml-2"
+                  size={"xs"}
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NoScrtWarning;
