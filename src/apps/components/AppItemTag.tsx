43266d580318a9641430c9da14f9b83cc0d481fe
Kevin Rieger <mail@kevinrieger.com>
2023-05-12 12:21:41 +0200

    updated logos and refreshed /apps theme

--- src/apps/components/AppItemTag.tsx
+++ src/apps/components/AppItemTag.tsx
@@ -7,7 +7,7 @@ interface ITagProps {
 class AppTileTag extends React.Component<ITagProps> {
   render() {
     return (
-      <div className="bg-neutral-200 dark:bg-neutral-900 text-xs font-semibold px-2 py-1 rounded-md inline-block transition-colors">
+      <div className="bg-neutral-200 dark:bg-neutral-900 text-xs font-semibold px-2 py-1 rounded-md inline-block transition-colors ">
         {this.props.name}
       </div>
     );
