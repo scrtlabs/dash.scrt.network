7cab098a3c229ade0bb08e8d0eeb028ed2c27c00
Kevin Rieger <mail@kevinrieger.com>
2023-11-06 15:32:03 +0100

    fixed light mode

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -62,7 +62,7 @@ export function Navigation({
             to="/"
             className={({ isActive }) =>
               isActive
-                ? 'text-black dark:text-white bg-gradient-to-r from-neutral-200 via-neutral-200 to-neutral-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
+                ? 'text-black dark:text-white bg-gradient-to-r from-gray-200 via-gray-200 to-gray-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
                 : 'text-neutral-700 dark:text-neutral-300 cursor-pointer hover:text-black dark:hover:text-white block w-full px-8 py-3 rounded-xl transition-colors'
             }
           >
@@ -75,7 +75,7 @@ export function Navigation({
             to="/ibc"
             className={({ isActive }) =>
               isActive
-                ? 'text-black dark:text-white bg-gradient-to-r from-neutral-200 via-neutral-200 to-neutral-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
+                ? 'text-black dark:text-white bg-gradient-to-r from-gray-200 via-gray-200 to-gray-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
                 : 'text-neutral-700 dark:text-neutral-300 cursor-pointer hover:text-black dark:hover:text-white block w-full px-8 py-3 rounded-xl transition-colors'
             }
           >
@@ -88,7 +88,7 @@ export function Navigation({
             to="/wrap"
             className={({ isActive }) =>
               isActive
-                ? 'text-black dark:text-white bg-gradient-to-r from-neutral-200 via-neutral-200 to-neutral-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
+                ? 'text-black dark:text-white bg-gradient-to-r from-gray-200 via-gray-200 to-gray-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
                 : 'text-neutral-700 dark:text-neutral-300 cursor-pointer hover:text-black dark:hover:text-white block w-full px-8 py-3 rounded-xl transition-colors'
             }
           >
@@ -101,7 +101,7 @@ export function Navigation({
             to="/bridge"
             className={({ isActive }) =>
               isActive
-                ? 'isActiveNavLink text-black dark:text-white bg-gradient-to-r from-neutral-200 via-neutral-200 to-neutral-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
+                ? 'isActiveNavLink text-black dark:text-white bg-gradient-to-r from-gray-200 via-gray-200 to-gray-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
                 : 'isInactiveNavLink text-neutral-700 dark:text-neutral-300 cursor-pointer hover:text-black dark:hover:text-white block w-full px-8 py-3 rounded-xl transition-colors'
             }
           >
@@ -123,7 +123,7 @@ export function Navigation({
             to="/get-scrt"
             className={({ isActive }) =>
               isActive
-                ? 'isActiveNavLink text-black dark:text-white bg-gradient-to-r from-neutral-200 via-neutral-200 to-neutral-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
+                ? 'isActiveNavLink text-black dark:text-white bg-gradient-to-r from-gray-200 via-gray-200 to-gray-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
                 : 'isInactiveNavLink text-neutral-700 dark:text-neutral-300 cursor-pointer hover:text-black dark:hover:text-white block w-full px-8 py-3 rounded-xl transition-colors'
             }
           >
@@ -136,7 +136,7 @@ export function Navigation({
             to="/staking"
             className={({ isActive }) =>
               isActive
-                ? 'text-black dark:text-white bg-gradient-to-r from-neutral-200 via-neutral-200 to-neutral-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
+                ? 'text-black dark:text-white bg-gradient-to-r from-gray-200 via-gray-200 to-gray-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
                 : 'text-neutral-700 dark:text-neutral-300 cursor-pointer hover:text-black dark:hover:text-white block w-full px-8 py-3 rounded-xl transition-colors'
             }
           >
@@ -149,7 +149,7 @@ export function Navigation({
             to="/send"
             className={({ isActive }) =>
               isActive
-                ? 'text-black dark:text-white bg-gradient-to-r from-neutral-200 via-neutral-200 to-neutral-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
+                ? 'text-black dark:text-white bg-gradient-to-r from-gray-200 via-gray-200 to-gray-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
                 : 'text-neutral-700 dark:text-neutral-300 cursor-pointer hover:text-black dark:hover:text-white block w-full px-8 py-3 rounded-xl transition-colors'
             }
           >
@@ -162,7 +162,7 @@ export function Navigation({
             to="/apps"
             className={({ isActive }) =>
               isActive
-                ? 'isActiveNavLink text-black dark:text-white bg-gradient-to-r from-neutral-200 via-neutral-200 to-neutral-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
+                ? 'isActiveNavLink text-black dark:text-white bg-gradient-to-r from-gray-200 via-gray-200 to-gray-200/10 dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700/10 block w-full px-8 py-3 rounded-xl transition-colors font-semibold cursor-default'
                 : 'isInactiveNavLink text-neutral-700 dark:text-neutral-300 cursor-pointer hover:text-black dark:hover:text-white block w-full px-8 py-3 rounded-xl transition-colors'
             }
           >
