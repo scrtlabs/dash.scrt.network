c83bb36460c78a6235753d3b1ca890649ca481be
h3rt <94856309+SecretSaturn@users.noreply.github.com>
2024-02-10 13:41:26 +0100

    Swap evmosjs

--- vite.config.ts
+++ vite.config.ts
@@ -16,10 +16,5 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000
-  },
-  build: {
-    rollupOptions: {
-      external: ['@buf/evmos_evmos.bufbuild_es/evmos/vesting/v1/tx_pb.js']
-    }
   }
 })
