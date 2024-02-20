a22512d72fb05fd295570e4278f95c858fd7ae9a
h3rt <94856309+SecretSaturn@users.noreply.github.com>
2024-02-20 03:51:35 +0100

    Add Swing

--- src/pages/bridge/SquidModal.tsx
+++ src/pages/bridge/SquidModal.tsx
@@ -117,7 +117,7 @@ export default function SquidModal(props: Props) {
       {/* Outer */}
       <div
         className="fixed top-0 left-0 right-0 bottom-0 bg-black/80 dark:bg-black/80 z-50 flex items-center justify-center"
-        onClick={props.onClose}
+        //onClick={props.onClose}
       >
         {/* Inner */}
         <div className="relative w-full onEnter_fadeInDown h-full overflow-scroll scrollbar-hide flex items-center justify-center">
