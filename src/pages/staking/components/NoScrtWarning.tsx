16d9257be1bf574c3b9de69b7cd28a693bf84b31
Kevin Rieger <mail@kevinrieger.com>
2024-03-22 17:37:23 +0100

    enhanced app tiles and noScrtWarning

--- src/pages/staking/components/NoScrtWarning.tsx
+++ src/pages/staking/components/NoScrtWarning.tsx
@@ -1,34 +1,38 @@
-import { faArrowUpRightFromSquare, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUpRightFromSquare, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function NoScrtWarning() {
   return (
     <>
-      <div className="px-4 mb-4 max-w-6xl mx-auto">
-        <div className="inline-block w-full md:w-auto bg-yellow-800/10 dark:bg-yellow-800/40 border border-yellow-700 dark:border-yellow-600 p-4 rounded-lg">
-          <div className="font-semibold text-yellow-700 dark:text-yellow-600 flex flex-row items-center">
-            <FontAwesomeIcon icon={faTriangleExclamation} className="mr-3" />
-            <span>{`You do not have any SCRT for Staking`}</span>
+      <div className="px-4 mb-4 max-w-6xl mx-auto text-sm">
+        <div className="w-full dark:bg-neutral-800 p-4 rounded-lg">
+          <div className="flex gap-4 text-yellow-800 dark:text-yellow-500">
+            <FontAwesomeIcon icon={faInfoCircle} className="mt-[3px]" />
+            <div>
+              <div className="font-semibold flex flex-row items-center">
+                <span>{`You do not have any SCRT for Staking`}</span>
+              </div>
+              <ul className="list-disc mt-3 ml-4 flex flex-col gap-1">
+                <li>
+                  <a href="/get-scrt" className="hover:underline">
+                    Get SCRT
+                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="text-xs ml-2" size={'xs'} />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://medium.com/secret-network-ecosystem-and-technology/how-to-get-store-and-stake-scrt-64ae2740b98e"
+                    className="hover:underline"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Learn more about Staking
+                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="text-xs ml-2" size={'xs'} />
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <ul className="list-disc ml-4 flex flex-col gap-0.5 text-yellow-700 dark:text-yellow-600">
-            <li>
-              <a href="/get-scrt" className="hover:border-b">
-                Get SCRT
-                <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="text-xs ml-2" size={'xs'} />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://medium.com/@secretnetwork/how-to-get-store-and-stake-scrt-64ae2740b98e"
-                className="hover:border-b"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn more about Staking
-                <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="text-xs ml-2" size={'xs'} />
-              </a>
-            </li>
-          </ul>
         </div>
       </div>
     </>
