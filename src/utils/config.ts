64d2f79859edb515a79ee7fa56201ff6404a3a90
h3rt <94856309+SecretSaturn@users.noreply.github.com>
2024-04-09 16:26:05 +0200

    Add NLS (#151)
    * add NLS token
    
    * Fix channel-ids
    
    * Add SNLS contract

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -317,10 +317,11 @@ export const chains: { [chain_name: string]: Chain } = {
   },
   Nolus: {
     chain_name: 'Nolus',
-    deposit_channel_id: 'channel-XX',
+    deposit_channel_id: 'channel-13995',
     deposit_gas: 200_000,
     deposit_gas_denom: 'unls',
-    withdraw_channel_id: 'channel-783',
+    withdraw_channel_id: 'channel-146',
+    //withdraw_channel_id: 'channel-783',
     withdraw_gas: 150_000,
     chain_id: 'pirin-1',
     bech32_prefix: 'nolus',
@@ -1681,6 +1682,35 @@ export const tokens: Token[] = [
             }
           ],
           'transfer/channel-2/4'
+        )
+      }
+    ]
+  },
+  {
+    name: 'NLS',
+    description: 'Nolus Governance Token',
+    address: 'secret1yafpcu9wpauy5ktymggzk9kmsvmce0hkl9p2h7',
+    code_hash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+    image: '/nolus.svg',
+    decimals: 6,
+    coingecko_id: 'nolus',
+    deposits: [
+      {
+        chain_name: 'Nolus',
+        denom: 'unls'
+      }
+    ],
+    withdrawals: [
+      {
+        chain_name: 'Nolus',
+        denom: ibcDenom(
+          [
+            {
+              incomingChannelId: chains['Nolus'].withdraw_channel_id,
+              incomingPortId: 'transfer'
+            }
+          ],
+          'unls'
         )
       }
     ]
