2b5d8d5e39f4b3749c33a85111258d2619a05114
Kevin Rieger <mail@kevinrieger.com>
2024-04-15 22:10:19 +0200

    added euro value for claimableRewards

--- src/pages/staking/components/StakingStats/ClaimableRewards.tsx
+++ src/pages/staking/components/StakingStats/ClaimableRewards.tsx
@@ -1,9 +1,34 @@
+import BigNumber from 'bignumber.js'
 import Button from 'components/UI/Button/Button'
+import { APIContext } from 'context/APIContext'
 import { StakingContext } from 'pages/staking/Staking'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { useTokenPricesStore } from 'store/TokenPrices'
+import { useUserPreferencesStore } from 'store/UserPreferences'
+import { Nullable } from 'types/Nullable'
+import { toCurrencyString } from 'utils/commons'
+import { scrtToken } from 'utils/tokens'
 
 function ClaimableRewards() {
   const { setIsClaimRewardsModalOpen, totalPendingRewards } = useContext(StakingContext)
+  const { convertCurrency } = useContext(APIContext)
+  const { getValuePrice, priceMapping } = useTokenPricesStore()
+  const { currency } = useUserPreferencesStore()
+
+  const [claimableRewardsInUsd, setClaimableRewardsInUsd] = useState<string>('')
+
+  useEffect(() => {
+    if (priceMapping !== null && totalPendingRewards !== null) {
+      const valuePrice = getValuePrice(scrtToken, BigNumber(totalPendingRewards))
+      if (valuePrice) {
+        const priceInCurrency = convertCurrency('USD', valuePrice, currency)
+        if (priceInCurrency !== null) {
+          setClaimableRewardsInUsd(toCurrencyString(priceInCurrency, currency))
+        }
+      } else {
+      }
+    }
+  }, [priceMapping, totalPendingRewards])
 
   function openClaimRewardsModal() {
     setIsClaimRewardsModalOpen(true)
@@ -18,8 +43,8 @@ function ClaimableRewards() {
           <span className="text-xs font-semibold text-neutral-400"> SCRT</span>
         </div>
         <div className="text-xs text-neutral-400 font-medium font-mono">
-          {false ? (
-            <>{`$0.00`}</>
+          {true ? (
+            <>{claimableRewardsInUsd}</>
           ) : (
             <div className="animate-pulse inline-block">
               <div className="h-5 w-12 bg-white dark:bg-neutral-700 rounded-xl"></div>
